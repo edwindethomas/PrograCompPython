3d07f623d8fca572425bf6a9227a7149715c2634
Edwin <edwin.dthomas27@gmail.com>
2021-03-05 16:01:19 -0600

    se agrega el problema 1030 de URI

---
+++ URI/URI1030.py
@@ -0,0 +1,15 @@
+from collections import deque
+def joseph(n,k):
+    d = deque(n)
+    while len(d)>1:
+        d.rotate(-k)
+        d.pop()
+    return(d.pop())
+
+nc = int(input())
+for i in range(nc):
+    n,k = map(int,input().split())
+    n = [x for x in range(1,n+1)]
+    k = int(k)
+    print(f"Case {i+1}: {joseph(n,k)}")
+ 
